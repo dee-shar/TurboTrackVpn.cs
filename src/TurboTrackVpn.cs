77be7d4544a4a8a4e6a327a98411c4a7ae92856b
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-25 19:45:13 +0300

    Update and rename turbo_track_vpn.sh to TurboTrackVpn.cs

---
+++ src/TurboTrackVpn.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace TurboTrackVpnApi
+{
+    public class TurboTrackVpn
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://api.turbotrackvpn.com/api/v1";
+        public TurboTrackVpn()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("okhttp/3.14.9");
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<string> getServers(string uuid)
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/servers?uuid={uuid}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
